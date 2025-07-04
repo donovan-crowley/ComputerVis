cee5c547f0c5cd61410c801001fda76129870ace
Donovan Crowley <donocrowley16@gmail.com>
2025-07-03 20:49:26 -0400

    Add searching circles using a hough circle transform dependent on HSV values

--- src/ObjectTracker.cpp
+++ src/ObjectTracker.cpp
@@ -22,34 +22,73 @@ void ObjectTracker::run() {
 	int lastLowV = 0;
 	int lastHighV = 0;
 
+	// My values
+	// Green Circle
+	int lowH = 50;
+	int highH = 70;
+	int lowS = 100;
+	int highS = 255;
+	int lowV = 100;
+	int highV = 255;
+
 	int state = SEARCH;
 	
-	Mat originalImg;
-	Point originalP(0, 0);
+	Mat originalImg, displayImg;
+	Point lastCenter = Point(0, 0);
 	int lastRadius = 200;
 
 	
 	while(true){
-		flip(originalImg, originalImg, 1);
-			
-		// test frames of the image
-		bool testing = cap.read(originalImg);
-		if(!testing){
-			cout << "Failure to read frames from video stream" << endl;
-			break;
+		if(state != TRAIN){
+			// Test frames of the image
+			bool testing = cap.read(originalImg);
+			flip(originalImg, originalImg, 1);
+
+			if(!testing){
+				cout << "Failure to read frames from video stream" << endl;
+				break;
+			}
 		}
 		
-		Mat circleImg;
+		Mat circleImg = Mat::zeros(originalImg.size(), CV_8UC3);
+
 		if(state == SEARCH){
-			circleImg = Mat::zeros(originalImg.size(), CV_8UC3);
-			circle(circleImg, originalP, lastRadius, Scalar(0, 255, 0), 2);
+			vector<Vec3f> circles;
+			Mat imgGray;
+			cvtColor(originalImg, imgGray, COLOR_BGR2GRAY);
+			HoughCircles(imgGray, circles, HOUGH_GRADIENT, 2, imgGray.rows * 2, 250, 125, 0, 0);
+
+			for(size_t i = 0; i < circles.size(); i++){
+				Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+				int radius = cvRound(circles[i][2]);
+				circle(circleImg, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+				circle(circleImg, center, radius, Scalar(0, 0, 255), 5, 16, 0);
+				lastCenter = center;
+				lastRadius = radius;
+			}
+
+			displayImg = originalImg + circleImg;
+		}
+		else if(state == TRAIN){
+
+		}
+		else if(state == TRACK){
+
 		}
 
-		Mat imgDisplay = originalImg + circleImg;
+		imshow("Video Stream", displayImg);
+		int key = waitKey(1);
 
-		imshow("Video Stream", imgDisplay);
+		// Training
+		if(key == 116){
+			if(state == SEARCH){
+				state = TRAIN;
+			} else{
+				state = SEARCH;
+			}
+		}
 
-		if(waitKey(1) == 27) break;
+		if(key == 27) break;
 	}
 	cap.release();
 	destroyAllWindows();
